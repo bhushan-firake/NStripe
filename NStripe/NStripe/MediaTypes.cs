7a4338d965828d6e34b06c0d27dd4b2bf2f9dfaa
Bhushan Firake <b.bhushan21@gmail.com>
2015-10-20 21:42:50 +0530

    Defined media types for request and accept

---
+++ NStripe/NStripe/MediaTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NStripe
+{
+    internal static class MediaTypes
+    {
+        public const string Json = "application/json";
+        public const string FormUrlEncoded = "application/x-www-form-urlencoded";
+    }
+}
