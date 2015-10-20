7d09b63f3c5de2f2b5d95f90fc7d937bff07d26c
Bhushan Firake <b.bhushan21@gmail.com>
2015-10-20 21:27:36 +0530

    Added baseclass for response

---
+++ NStripe/NStripe/IResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NStripe
+{
+    internal interface IResponse
+    {
+    }
+
+    internal interface IResponse<T> : IResponse
+    {
+    }
+}
