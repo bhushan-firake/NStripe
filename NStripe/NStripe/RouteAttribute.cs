297a08771a24b591813a663afed38cd22aca30b9
Bhushan Firake <b.bhushan21@gmail.com>
2015-10-20 21:31:16 +0530

    Added baseclass for response

---
+++ NStripe/NStripe/RouteAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NStripe
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    internal class RouteAttribute : Attribute
+    {
+        public RouteAttribute(string path)
+            : this(path, null)
+        {
+        }
+
+        public RouteAttribute(string path, string verbs)
+        {
+            this.Path = path;
+            this.Verbs = verbs;
+        }
+
+        public string Path { get; set; }
+        public string Verbs { get; set; }
+    }
+}
