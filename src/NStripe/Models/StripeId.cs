df0a01700b4587353edbcaa058b4804294b3fae6
Bhushan Firake <b.bhushan21@gmail.com>
2015-10-24 00:06:47 +0530

    Removed unwanted references

--- src/NStripe/Models/StripeId.cs
+++ src/NStripe/Models/StripeId.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-
-namespace NStripe
+﻿namespace NStripe
 {
     public class StripeId : StripeObject
     {
--- src/NStripe/Models/StripeObject.cs
+++ src/NStripe/Models/StripeObject.cs
@@ -1,16 +1,4 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-
-namespace NStripe
+﻿namespace NStripe
 {
     public class StripeObject
     {
