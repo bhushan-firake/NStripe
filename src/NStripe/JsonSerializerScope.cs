6db82899cbfa46482fc38a0725ac25cc9d233d2c
Bhushan Firake <b.bhushan21@gmail.com>
2015-10-24 09:46:29 +0530

    Reduced SerializerScope

--- src/NStripe/JsonSerializerScope.cs
+++ src/NStripe/JsonSerializerScope.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace NStripe
 {
-    public class JsonSerializerScope : IDisposable
+    internal class JsonSerializerScope : IDisposable
     {
         private readonly Func<JsonSerializerSettings> holdSerializerSettings;
 
