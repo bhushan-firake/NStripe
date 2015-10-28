1736a017012bbc48fb4247cbc5309ef36ef3e785
Bhushan Firake <b.bhushan21@gmail.com>
2015-10-28 08:38:40 +0530

    Added stripediscount cancellation methods

--- src/NStripe/Models/StripeDiscount.cs
+++ src/NStripe/Models/StripeDiscount.cs
@@ -2,12 +2,29 @@
 
 namespace NStripe
 {
-    //TODO:Recheck
+    #region Request
+
+    [Route("/customers/{customerId}/discount", "DELETE")]
+    public class DeleteCustomerDiscount : StripeRequestBase, IResponse<StripeDiscount>
+    {
+        public string CustomerId { get; set; }
+    }
+
+    [Route("/customers/{customerId}/subscriptions/{subscriptionId}", "DELETE")]
+    public class DeleteSubscriptionDiscount : StripeRequestBase, IResponse<StripeDiscount>
+    {
+        public string CustomerId { get; set; }
+        public string SubscriptionId { get; set; }
+    }
+
+    #endregion Request
+
     public class StripeDiscount : StripeId
     {
         public string Customer { get; set; }
         public StripeCoupon Coupon { get; set; }
         public DateTime? Start { get; set; }
         public DateTime? End { get; set; }
+        public string Subscription { get; set; }
     }
 }
